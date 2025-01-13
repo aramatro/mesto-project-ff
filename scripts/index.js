d12e1340a9010151a0a7665eed8f804dfae27bc9
Tamara Rozenfeld <aramatro@yandex.ru>
2025-01-13 12:14:25 +0300

    add js code

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,24 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const cardContainer = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+function createCard(item, deleteCard) {
+  const cardElement = cardTemplate.cloneNode(true);
 
-// @todo: Функция создания карточки
+  cardElement.querySelector('.card__title').textContent = item.name;
+  cardElement.querySelector('.card__image').src = item.link;
 
-// @todo: Функция удаления карточки
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', () =>
+    deleteCard(deleteButton.closest(`.places__item`))
+  );
 
-// @todo: Вывести карточки на страницу
+  return cardElement;
+};
+
+function deleteCard(item) {
+  item.remove();
+};
+
+initialCards.forEach((item) => {
+  cardContainer.append(createCard(item, deleteCard));
+});
