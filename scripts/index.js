1485c1cb60ba5e2e8ec12949f98d12417c47ee4f
Tamara Rozenfeld <aramatro@yandex.ru>
2025-01-13 13:16:22 +0300

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,18 @@
 const cardTemplate = document.querySelector('#card-template').content;
 const cardContainer = document.querySelector('.places__list');
 
-function createCard(item, deleteCard) {
-  const cardElement = cardTemplate.cloneNode(true);
+function createCard(cardData, deleteCard) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-  cardElement.querySelector('.card__title').textContent = item.name;
-  cardElement.querySelector('.card__image').src = item.link;
+  cardElement.querySelector('.card__title').textContent = cardData.name;
+
+  const cardImage= cardElement.querySelector('.card__image');
+  cardImage.alt = cardData.name;
+  cardImage.src = cardData.link;
 
   const deleteButton = cardElement.querySelector('.card__delete-button');
   deleteButton.addEventListener('click', () =>
-    deleteCard(deleteButton.closest(`.places__item`))
+    deleteCard(cardElement)
   );
 
   return cardElement;
